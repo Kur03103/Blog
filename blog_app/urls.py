24856c39b1e941757b11ec47d46976b4e4235c1f
Kur03103 <aayushaj3103@gmail.com>
2025-11-12 16:47:35 +0545

    Updated blog content

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from blog_app import views
-
 urlpatterns = [
-    path("", views.post_list, name="post-list"),
+    path('', views.post_list, name='post-list'),
     path("post-detail/<int:pk>/", views.post_detail, name="post-detail"),
     path("draft-list/", views.draft_list, name="draft-list"),
+    path("draft-detail/<int:pk>/", views.draft_detail, name = "draft-detail"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -2,23 +2,20 @@ from django.shortcuts import render
 from blog_app.models import Post
 
 # Create your views here.
-
 def post_list(request):
     posts = Post.objects.filter(published_at__isnull=False)
     return render(
-        request,
-        "post_list.html",
-        {"posts" : posts}
-    )
-
-def post_detail(request, pk):
+        request, 
+        'post_list.html', 
+        {"posts": posts},
+        )
+def post_detail(request,pk):
     post = Post.objects.get(pk=pk)
     return render(
         request,
         "post_detail.html",
-        {"post":post},
+        {"post": post},
     )
-
 def draft_list(request):
     posts = Post.objects.filter(published_at__isnull=True)
     return render(
@@ -26,4 +23,10 @@ def draft_list(request):
         "draft_list.html",
         {"posts": posts},
     )
-
+def draft_detail(request, pk):
+    post = Post.objects.get(pk=pk,published_at__isnull=True)
+    return render(
+        request,
+        "draft_detail.html",
+        {"post": post},
+    )
